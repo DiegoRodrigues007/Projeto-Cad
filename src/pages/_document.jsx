de4807829b61636e90e4a65fce0c700c91f353d3
Diego <diego82018201@gmail.com>
2023-11-26 20:35:24 -0300

    alteração ico

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <title>CAD</title>
-        <link rel="icon" href="/Assets/img/favicon.png" />
+        <link rel="icon" href="/Assets/img/icon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet"/>
       </Head>
